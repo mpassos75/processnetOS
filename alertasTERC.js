196589abbd164143a23bb8866f8663157702a414
mpassos75 <149718944+mpassos75@users.noreply.github.com>
2023-11-24 09:50:22 -0300

    Add files via upload

---
+++ alertasTERC.js
@@ -0,0 +1,7 @@
+
+
+function idEquip(){
+	
+  document.getElementById("logEquip").innerHTML += '<br>EQUIPE_TERC-SR<br>';
+  document.getElementById( "logEquip" ).setAttribute("style", "color: red; font-size: 15px; font-family: Courier;");
+}
